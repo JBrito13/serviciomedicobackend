51947510ae4c918ad9ffede3bf9d24ffb91000a6
JBrito13 <juan.brito@urbe.edu>
2023-11-28 19:32:38 -0400

    cambios en el reporte de morbilidad

--- app/Exports/MorbilidadExport.php
+++ app/Exports/MorbilidadExport.php
@@ -2,6 +2,7 @@
 
 namespace App\Exports;
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
@@ -24,12 +25,15 @@ class MorbilidadExport implements FromCollection, WithHeadings
     public function collection()
     {
 
+        $fechaInicio = Carbon::parse($this->fecha_inicio)->format('Y-m-d H:i:s');
+        $fechaFin = Carbon::parse($this->fecha_fin)->format('Y-m-d H:i:s');
+
         $results = DB::select("
             SELECT sd.descripcion_diagnostico, COUNT(st.id_diagnostico) AS cantidad
             FROM sm_consultas sc, sm_tratamientos st, sm_diagnosticos sd
             WHERE sc.id_consulta = st.id_consulta
             AND st.id_diagnostico = sd.id_diagnostico
-            AND sc.fecha_consulta BETWEEN '$this->fecha_inicio' AND '$this->fecha_fin'
+            AND sc.fecha_consulta BETWEEN '$fechaInicio' AND '$fechaFin'
             GROUP BY sd.id_diagnostico
             ORDER BY cantidad DESC
         ");
